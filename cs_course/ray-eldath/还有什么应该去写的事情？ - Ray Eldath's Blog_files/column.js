9e43a0879e7d8bd70f0ff28141d985e0f1fd6249
上田光華 <masswie@foxmail.com>
2022-10-28 18:47:34 +1100

    xiu

---
+++ cs_course/ray-eldath/还有什么应该去写的事情？ - Ray Eldath's Blog_files/column.js
@@ -0,0 +1,2 @@
+(function(){function $(){return Array.prototype.slice.call(document.querySelectorAll.apply(document,arguments));}
+if($('.columns .column-right').length&&$('.columns .column-right-shadow').length&&!$('.columns .column-right-shadow')[0].children.length){for(const child of $('.columns .column-right')[0].children){$('.columns .column-right-shadow')[0].append(child.cloneNode(true));}}}());
